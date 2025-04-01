c764b692632c71ad9f09f01fecdf1950199fb3e7
Vera <vera.martins152@gmail.com>
2025-04-01 16:07:53 +0100

    Create a query to list the list of employees per manager

---
+++ find_manager_cycles.sql
@@ -0,0 +1,33 @@
+USE memory.default;
+
+WITH RECURSIVE MANAGERS (manager_id, employee_id, employee_ids_list) AS (
+    SELECT 
+        manager_id
+        , employee_id
+        , cast(employee_id as VARCHAR) AS employee_ids_list
+    FROM EMPLOYEE
+
+    UNION ALL
+
+    SELECT 
+        m.manager_id
+        , e.employee_id
+        , concat(m.employee_ids_list, ', ', cast(e.employee_id as VARCHAR)) AS employee_ids_list
+    FROM EMPLOYEE e 
+        INNER JOIN MANAGERS m
+            ON m.employee_id = e.manager_id
+    WHERE
+        position(cast(e.employee_id as VARCHAR) in m.employee_ids_list) = 0
+
+)
+
+SELECT
+    manager_id
+    , max(employee_ids_list) AS employee_ids_list
+FROM MANAGERS
+    WHERE
+        position(cast(manager_id as VARCHAR) in employee_ids_list) > 0
+GROUP BY 
+    manager_id
+ORDER BY 
+    manager_id;
